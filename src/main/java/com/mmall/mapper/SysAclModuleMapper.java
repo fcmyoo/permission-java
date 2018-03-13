9843d8ea94b5c5f674b9971f67df8fef5b44ff9b
lee <leewin110@126.com>
2018-03-13 11:55:30 +0800

    搭建框架及书写Helper

---
+++ src/main/java/com/mmall/mapper/SysAclModuleMapper.java
@@ -0,0 +1,18 @@
+package com.mmall.mapper;
+
+import com.mmall.model.SysAclModule;
+
+public interface SysAclModuleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysAclModule record);
+
+    int insertSelective(SysAclModule record);
+
+    SysAclModule selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysAclModule record);
+
+    int updateByPrimaryKey(SysAclModule record);
+
+}
